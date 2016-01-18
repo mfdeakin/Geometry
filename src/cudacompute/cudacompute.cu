c9e5e29d741ee3c036fad3b23191e601ef31e39b
Michael Deakin <mfdeakin@cs.unc.edu>
2016-01-18 14:00:09 -0500

    Started work on Cuda line-quadric intersection computation Started working on creating sets of difficult problems in genshapes.py

---
+++ src/cudacompute/cudacompute.cu
@@ -0,0 +1,12 @@
+
+#include "cudadef.h"
+
+#include "line.hpp"
+#include "quadrics.hpp"
+#include "accurate_intersections.hpp"
+
+__global__ void computeIntersections() {}
+
+int main(int argc, char **argv) {
+	return 0;
+}
