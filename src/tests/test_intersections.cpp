8233000013af221e752dc28ada2ed23f56a13d6a
mfdeakin <mdever44@gmail.com>
2016-06-07 15:47:13 -0400

    Renamed the determinant test to the resultant determinant test Removed extraneous code from the resultant determinant test

--- src/tests/test_intersections.cpp
+++ src/tests/test_intersections.cpp
@@ -1,18 +1,20 @@
 
 #include <array>
-#include <list>
 #include <fstream>
+#include <list>
 
 #include <gtest/gtest.h>
 
-#include "quadric.hpp"
-#include "origin.hpp"
 #include "accurate_intersections_approx.hpp"
 #include "accurate_intersections_incprec.hpp"
+#include "accurate_intersections_multi.hpp"
 #include "accurate_intersections_resultant.hpp"
+
 #include "accurate_math.hpp"
-#include "timer.hpp"
 #include "genericfp.hpp"
+#include "origin.hpp"
+#include "quadric.hpp"
+#include "timer.hpp"
 
 using fptype = float;
 constexpr const int dim = 3;
@@ -25,8 +27,10 @@ using IF =
     Geometry::IntersectionApproximate<dim, fptype, fptype>;
 using II = Geometry::IntersectionIncreasedPrec<dim, fptype>;
 using IR = Geometry::IntersectionResultant<dim, fptype>;
+using IM =
+    Geometry::IntersectionResultantMulti<dim, fptype>;
 
-TEST(QLIntersect, Determinant) {
+TEST(QLIntersect, ResultantDeterminant) {
   constexpr const int numTests = 1;
   /* For simplicity, just use to spheres.
    * Since the line is axis aligned,
@@ -34,40 +38,32 @@ TEST(QLIntersect, Determinant) {
    * root of the constant coefficient
    */
   constexpr const int numQuads = 2;
-  constexpr const fptype
-      quadCoeffs[numTests][numQuads][Q::numCoeffs] = {{
-          {1.0, 1.0, 1.0, -1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-           0.0},
-          {1.0, 1.0, 1.0, -1.25, 0.0, 0.0, 0.0, 0.0, 0.0,
-           0.0},
-      }};
+  constexpr const fptype quadCoeffs[numTests][numQuads]
+                                   [Q::numCoeffs] = {{
+                                       {1.0, 1.0, 1.0, -1.0,
+                                        0.0, 0.0, 0.0, 0.0,
+                                        0.0, 0.0},
+                                       {1.0, 1.0, 1.0,
+                                        -1.25, 0.0, 0.0,
+                                        0.0, 0.0, 0.0, 0.0},
+                                   }};
   for(int t = 0; t < numTests; t++) {
     P intercept(V({0.0, 0.0, 0.0}));
     L l(intercept, V({1.0, 0.0, 0.0}));
     constexpr const fptype expectedDet = 0.0625;
 
-    IF fpInt[numQuads];
-    II mpInt[numQuads];
     IR resInt[numQuads];
     for(int i = 0; i < numQuads; i++) {
-      fpInt[i].l = l;
-      mpInt[i].l = l;
       resInt[i].l = l;
 
-      fpInt[i].intPos = MathFuncs::MathFuncs<fptype>::sqrt(
+      resInt[i].intPos = MathFuncs::MathFuncs<fptype>::sqrt(
           -quadCoeffs[t][i][3]);
-      mpInt[i].intPos = fpInt[i].intPos;
-      resInt[i].intPos = fpInt[i].intPos;
 
-      fpInt[i].otherIntPos =
+      resInt[i].otherIntPos =
           -MathFuncs::MathFuncs<fptype>::sqrt(
               -quadCoeffs[t][i][3]);
-      mpInt[i].otherIntPos = fpInt[i].intPos;
-      resInt[i].otherIntPos = fpInt[i].intPos;
 
       for(int j = 0; j < Q::numCoeffs; j++) {
-        fpInt[i].q.setCoeff(j, quadCoeffs[t][i][j]);
-        mpInt[i].q.setCoeff(j, quadCoeffs[t][i][j]);
         resInt[i].q.setCoeff(j, quadCoeffs[t][i][j]);
       }
     }
